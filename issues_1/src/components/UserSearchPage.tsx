cfff519ccf0075a73a2413713187d229126c658d
TanakaMiki-Altissima <tanaka.miki@altissima.jp>
2026-01-08 10:50:48 +0900

    user_seachページ作成

---
+++ issues_1/src/components/UserSearchPage.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export function UserSearchPage() {
+  return (
+    <div>
+      <h1>メンテナンスノート</h1>
+      <input placeholder="キーワード・電話番号で検索" />
+      <button>検索</button>
+    </div>
+  );
+}
